bfcf361c0b03efc9f6959fcae0c7dacf50a636f3
Aditi Tiwari <60311309+Aditi0310@users.noreply.github.com>
2022-03-11 20:47:15 +0530

    Time: 11 ms (45.28%), Space: 13.6 MB (79.26%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        //vector<int> dp(n+1);
+        
+        for(int i=2 ; i<n ; i++){
+            cost[i] = cost[i]+ min(cost[i-1], cost[i-2]); 
+        }
+        return min(cost[n-1], cost[n-2]);
+    }
+};
+
+// in this we have to check the min cost of last two stairs
