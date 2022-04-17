94c1ed6125b8fd086dd11616beb3e3e8462c58d0
Aditi Tiwari <60311309+Aditi0310@users.noreply.github.com>
2022-04-17 20:55:09 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (19.70%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode*& ans, TreeNode* root){
+        if(!root) return;
+        inorder(ans, root->left);
+        ans->right = new TreeNode(root->val);
+        ans = ans->right;
+        
+        inorder(ans, root->right);
+    }
+    
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode *temp;
+        TreeNode* ans = new TreeNode();
+        temp = ans;
+        inorder(ans, root);
+        return temp->right;
+        
+    }
+};
+
+
+//same as inorder, here we have to create a new node to store the value
+// we are using temp cause ans will iterate to the last node and temp will store the actual starting val.
